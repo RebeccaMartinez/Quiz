c1d56d658205da70b4aae378164a21d943d34418
Rebecca Martinez Galan <rebe.mart.gal@gmail.com>
2015-11-11 13:59:54 +0000

    funcionando con varias preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,17 +1,23 @@
-//GET /quizes/question
-
-exports.question = function(req, res) {
-	res.render('quizes/question', {pregunta: 'Capital de italia'});
+var Quiz = require('../models/quiz_model');
+var quiz = new Quiz();
+var current = quiz.randomQuestion();
+exports.index = function(req, res, next) {
+	res.render('index', { title: 'Quiz' });
 };
-
-//GET /quizes/answer
-
+exports.question = function(req,res) {
+	current = quiz.randomQuestion();
+	res.render('quizes/question', {pregunta: current.pregunta });
+};
 exports.answer = function(req, res) {
-	if(req.query.respuesta == 'Roma'){
-		res.render('quizes/answer', {respuesta: 'Correcta'});
-	}
-	else {
-		res.render('quizes/answer', {respuesta: 'Incorrecta'});
-	}
-
+	var c = 'Incorrecto';
+	if (current.respuesta(req.query.respuesta)) { c = 'Correcto'; }
+	res.render('quizes/answer', {respuesta: c})
 };
+exports.showAll = function(req,res) {
+	var aux = quiz.returnAllQuestions();
+	res.render('quizes/question',{ pregunta: aux });
+};
+exports.showQuestionById = function(req,res) {
+	questId = req.params.id;
+	res.render('quizes/question',{pregunta: quiz.returnAllQuestions()[questId]});
+}
